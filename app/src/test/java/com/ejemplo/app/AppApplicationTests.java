019eb67d49201f831dc2273f91dea23ae1fb079f
Rosario <rosariozambrana83@gmail.com>
2026-08-18 00:53:11 -0400

    Provocar fallo intencional en prueba

--- app/src/test/java/com/ejemplo/app/AppApplicationTests.java
+++ app/src/test/java/com/ejemplo/app/AppApplicationTests.java
@@ -2,12 +2,14 @@ package com.ejemplo.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 2, "Fallo provocado intencionalmente para la prueba");
 	}
 
 }
